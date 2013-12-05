28a6a0a531a47bc9a38dc726feb240bbf47de4a7
alekole <alekole@yandex.ru>
2013-12-05 14:31:26 +0400

    rename

--- MdxClient/DimensionProperty.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MdxClient
-{
-    internal class DimensionProperty
-    {
-        public string UniqueName { get; set; }
-        public string Value { get; set; }
-
-    }
-}
--- MdxClientPooled/Properties/AssemblyInfo.cs
+++ MdxClientPooled/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MdxClient")]
-[assembly: AssemblyDescription("Specialized ADOMD DB provider")]
+[assembly: AssemblyTitle("MdxClientPooled")]
+[assembly: AssemblyDescription("Specialized ADOMD DB provider with pooled connection")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("DynamicTyped")]
-[assembly: AssemblyProduct("MdxClient")]
+[assembly: AssemblyCompany("Torero Inc")]
+[assembly: AssemblyProduct("MdxClientPooled")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("542bd3cf-9756-4e45-8461-c8ba85165d4a")]
+[assembly: Guid("1254afd9-ef10-4016-997e-bbc7ac8a0839")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1302.20214.0")]
-[assembly: AssemblyFileVersion("1.1302.20214.0")]
+[assembly: AssemblyVersion("1.3.*")]
+[assembly: AssemblyFileVersion("1.3.*")]
