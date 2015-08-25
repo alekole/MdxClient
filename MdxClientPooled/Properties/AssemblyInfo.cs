d078261df6f6f7dae6ba38f45d766887b032c0e9
Oleg <oleg.alekseenko@gmail.com>
2015-08-25 10:51:04 +0300

    add Microsoft.AnalysisServices.AdomdClient.dll

--- MdxClientPooled/Properties/AssemblyInfo.cs
+++ MdxClientPooled/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -10,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Torero Inc")]
 [assembly: AssemblyProduct("MdxClientPooled")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,4 +32,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.3.*")]
-[assembly: AssemblyFileVersion("1.3.*")]
+//[assembly: AssemblyFileVersion("1.3.*")]
